7ff03661eb662e05459fc0dc697bac30a12cadae
Nina Mutty <ninamutty@gmail.com>
2016-08-24 12:36:26 -0700

    Code demo in class - pawns and chess

--- class_methods.rb
+++ class_methods.rb
@@ -26,13 +26,23 @@
 ###################################
 
 ## INSTANCE VARIABLES
+    ##ATTRIBUTES OF THAT OBJECT
+        #ex: name, username, password
 
 ## INSTANCE METHODS
+    ##BEHAVIORS OF THE OBJECT
+        #ex: make a post, send a message, like a post
 
-      #INITIALIZE METHOD
-
-
-## ATTRIBUTES
+#INITIALIZE METHOD
+    #Will run everytime you create a new method
+# class User
+#     def initialize
+#         @name
+#     end
+#     def add_username(name)
+#         @name = name
+#     end
+# end
 
 
 # class Order
@@ -62,7 +72,13 @@
 
 #Class methods are called directly by the class and not by an instance of the class.
 
-
+# class Kitten
+#     def self.say_meow
+#         return "meow" ##### ALWAYS RETURN SOMETHING!! DON'T PUTS INSIDE A METHOD ####
+#     end
+# end
+#
+# puts Kitten.say_meow
 
 
 
@@ -71,48 +87,57 @@
 ###################################
 
 
-# class Pawn
-#   attr_reader :position
-#   def initialize(position)
-#     @position = position
-#   end
-#
-#   # This is the class method, it starts with self.
-#   # It is only called on the class directly Pawn.make_row
-#   def self.make_row(side)
-#     if side == "white"
-#       num = 2
-#     else
-#       num = 7
-#     end
-#
-#     pawns = []
-#     ("a".."h").each do |letter|
-#       pawns << self.new("#{letter}#{num}")
-#     end
-#
-#     pawns
-#   end
-# end
-#
-# #make one pawn
-# one_pawn = Pawn.new("A2")
-#
-# #make a whole row of pawns
-# pawns = Pawn.make_row("black")
-#
-# #What is being stored in this local variable pawns?
-# print pawns
-#
-# #WHAT IS THIS DOING!?
-# puts pawns.shuffle.first.position
+class Pawn
+  attr_reader :position
+  def initialize(position)
+    @position = position
+  end
 
+  # This is the class method, it starts with self.
+  # It is only called on the class directly Pawn.make_row
+  # Cannot call on an instance of Pawn class (ex: pawn_one in line 116 below)
+  def self.make_row(side)
+    if side == "white"
+      num = 2
+    else
+      num = 7
+    end
 
+    pawns = []
+    ("a".."h").each do |letter|
+      pawns << self.new("#{letter}#{num}") ##Making a new object of itself within itself (spawning itself) #New objects of pawns are going into pawns
+    end
+
+    pawns
+  end
+end
+
+#make one pawn
+one_pawn = Pawn.new("A2")
+print one_pawn
+
+#make a whole row of pawns
+    ##Making a row of pawns based on the color - starts on a different side - creates an array of instances of pawns
+pawns = Pawn.make_row("black")
+
+#What is being stored in this local variable pawns?
+    ##A row of pawns on one of the sides of the board
+#print pawns
+pawns.each do |pawn|
+    puts pawn.position
+end
+
+#WHAT IS THIS DOING!?
+    ##This is randomly shuffling the array of pawns, picking the first one, and finding out it's position (so picking a random pawn in the row)
+puts pawns.shuffle.first.position
+
+#Pawn.new is a class method 
 
 ###################################
 ######   WHERE IS THIS USED?  #####
 ###################################
 #Class methods are for anything that does not deal with an individual instance of a class
+
 
 
 #In Gems, like faker
